4822cedd1cf8c3ad20512cdb37d8f70d62756dc7
hasnasalah <benjillali.hasna@gmail.com>
2025-11-26 17:11:51 -0500

    make changes for live server to work with assets

---
+++ dist/models/NetworkError.d.ts
@@ -0,0 +1,4 @@
+export declare class NetworkError extends Error {
+    constructor(message: string);
+}
+//# sourceMappingURL=NetworkError.d.ts.map
---
+++ dist/services/apiService.js
@@ -0,0 +1,57 @@
+import { Country } from "../models/Country.js";
+import { NetworkError } from "../models/NetworkError.js";
+import { DataError } from "../models/DataError.js";
+export async function getCountryData() {
+    const url = "https://restcountries.com/v3.1/all?fields=name,cca2,cca3,capital,tld,subregion,currencies,languages,borders,flags";
+    const url2 = "https://restcountries.com/v3.1/all?fields=population,region";
+    try {
+        const response = await fetch(url);
+        const respense2 = await fetch(url2);
+        if (!response.ok || !respense2.ok) {
+            throw new NetworkError(`Response status: ${response.status}`);
+        }
+        const result = await response.json();
+        const codesData = await respense2.json();
+        const countries = [];
+        for (const c of result) {
+            const nativeNames = c.name.nativeName ? Object.values(c.name.nativeName)[0] : null;
+            const topLevelDomain = c.tld ? c.tld.join(", ") : "";
+            const currencies = c.currencies ? Object.values(c.currencies).map((cur) => cur.name).join(", ") : "";
+            const languages = c.languages ? Object.values(c.languages).join(", ") : "";
+            const code = [c.cca2, c.cca3].join(", ");
+            let borderNames = [];
+            if (c.borders && Array.isArray(c.borders)) {
+                for (let borderCode of c.borders) {
+                    let borderName = borderCode;
+                    for (let r of result) {
+                        if (r.cca3 === borderCode) {
+                            borderName = r.name.official;
+                            break;
+                        }
+                    }
+                    borderNames.push(borderName);
+                }
+            }
+            const fullName = {
+                name: c.name.official,
+                nativeName: nativeNames?.official || c.name.official,
+            };
+            let population = 0;
+            let region = "";
+            for (const cd of codesData) {
+                population = cd.population;
+                region = cd.region;
+                break;
+            }
+            const country = new Country(fullName, population, region, c.subregion, c.capital, topLevelDomain, currencies, languages, borderNames, c.flags.png, code);
+            countries.push(country);
+        }
+        return countries;
+    }
+    catch (error) {
+        console.error(error.message);
+        throw new DataError("Failed to load country data");
+    }
+}
+;
+//# sourceMappingURL=apiService.js.map
